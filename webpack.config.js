38aae290a9a2597c408bbc6cef39882e0ba07d9e
Viswath <viswathleon@gmail.com>
2021-03-27 20:01:18 +0530

    base architecture implemented

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: [/dist/, /node_modules/],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|woff|woff2)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins:[
+      new HtmlWebpackPlugin ({
+        inject: true,
+        template: path.join(__dirname, '/public/index.html')
+      }),
+      new ForkTsCheckerWebpackPlugin()
+    ],
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
